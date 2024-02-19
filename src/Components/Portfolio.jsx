482e05df2674a50a69ad85bce8dbd1478da9e0e5
qaidjoharj53 <qaidjoharytqt@gmail.com>
2024-02-19 21:11:35 +0530

    image edits

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Table.jpg";
+import image from "../images/design-desk.jpeg";
 const imageAltText = "A desk with a plant, earpods, a locked smartphone, a pencil and a spectacle";
 
 import bgimage from "../images/projects.avif";
